8722c49867074cd69cc50007b5808fa3167e932a
marverix <marverix@users.noreply.github.com>
2025-09-20 05:49:22 +0000

    chore(version): Build v1.0.2

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,9 @@
+// Import and re-export main classes
 import { SongImporter } from "./SongImporter";
+// Import and re-export all factories
+export * from "./factory";
+// Import and re-export all models and types
+export * from "./model";
+// Export main classes
 export { SongImporter };
 //# sourceMappingURL=index.js.map

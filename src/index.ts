b63f512509e41129014fefde45bf5983f26e28e6
Marek Sierociński <mareksierocinski@gmail.com>
2025-09-20 07:48:54 +0200

    fix: Make index.ts export sudmodules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+// Import and re-export main classes
 import { SongImporter } from "./SongImporter";
 
+// Import and re-export all factories
+export * from "./factory";
+
+// Import and re-export all models and types
+export * from "./model";
+
+// Export main classes
 export {
   SongImporter
 };
